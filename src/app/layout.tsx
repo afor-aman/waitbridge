32decd40c113270aa6430b64de4ee93fdbda19a5
Aman Ali <amanali21100@gmail.com>
2025-11-22 23:40:41 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +25,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+				id="g-script1"
+				strategy="lazyOnload"
+				src="https://www.googletagmanager.com/gtag/js?id=G-7H0D20QJGJ"
+			></Script>
+			<Script id="g-script2" strategy="lazyOnload">
+				{`
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', 'G-7H0D20QJGJ');`}
+			</Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
