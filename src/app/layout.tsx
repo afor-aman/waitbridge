7b00b26737ac5b5ffd766314c77eaa1595569dd8
Aman Ali <amanali21100@gmail.com>
2025-12-02 23:00:02 +0530

    added clariyt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,17 @@ export default function RootLayout({
 							gtag('js', new Date());
 							gtag('config', 'G-7H0D20QJGJ');`}
 			</Script>
+      
+      <Script id="clarity-script" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ufbtp5j6xw");
+        `}
+      </Script>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
